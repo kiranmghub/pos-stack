d67e956dd5a48db2c1069b2d908f68a96f70956c
Kiran Madhela <kiranmadhela@MacBookPro.attlocal.net>
2025-10-06 12:38:08 -0500

    coupon feature..changes-2

--- pos-backend/discounts/urls.py
+++ pos-backend/discounts/urls.py
@@ -6,5 +6,9 @@ app_name = "discounts"
 
 urlpatterns = [
     path("active", ActiveDiscountRulesView.as_view(), name="active"),
+    path("active/", ActiveDiscountRulesView.as_view()),  # alias with slash
+
     path("coupon", CouponLookupView.as_view(), name="coupon"),
+    path("coupon/", CouponLookupView.as_view()),        # alias with slash
+
 ]
